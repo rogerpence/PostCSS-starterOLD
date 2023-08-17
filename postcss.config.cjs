7b4ee922ac78ddb7096d16d23606fd67b76c8e6b
Roger Pence <roger.pence@gmail.com>
2023-08-17 11:11:43 -0500

    before media.css

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,5 +1,7 @@
 const openProps = require("open-props");
 
+// console.log(openProps);
+
 module.exports = {
   map: { inline: true },
   plugins: [
@@ -7,7 +9,10 @@ module.exports = {
     require("postcss-nesting"),
     require("postcss-custom-media"),
     require("postcss-media-minmax"),
-    require("postcss-jit-props")(openProps),
+    require("postcss-jit-props")([openProps]),
+    // require("postcss-preset-env")({
+    //   "custom-media-queries": true,
+    // }),
     ...(process.env.NODE_ENV === "production" ? [require("cssnano")] : []),
   ],
 };
